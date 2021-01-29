e56bf0056f38fd343641f2590eb48fde9aeb3307
Luxi Wang <louis99@cs.washington.edu>
2021-01-29 00:48:42 -0800

    fix

--- tests/run.sh
+++ tests/run.sh
@@ -17,7 +17,7 @@ test02apk="tests/test02/app/build/outputs/apk/debug/app-debug.apk"
 
 # jadx_dir/bin/jadx tests/test01/app/build/outputs/apk/debug/app-debug.apk -d /tmp/jadx_apk_dir
 
-python scripts/search-res-xml.py /tmp/jadx_apk_dir tests/test01.xml
+# python scripts/search-res-xml.py /tmp/jadx_apk_dir tests/test01.xml
 
 python scripts/search-res-xml.py --apk $test01apk --src_path tests/test01 --markii_file_loc test01markii --jadx_file_dir /tmp/jadx_apk_dir1 --uix_path tests/test01.xml --run_markii F --run_jadx T
 
